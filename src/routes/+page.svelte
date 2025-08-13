a0351915ee937c001ece794bd3355cf78fe4a749
Matysse <mathis.jung@blgcloud.com>
2025-08-13 13:59:56 +0200

    init + barebone structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+    let randomNumber = $state(Math.floor(Math.random() * 100) + 1);
+    let userGuess = $state(0);
+
+    function resetGame() {
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+        userGuess = 0;
+    }
+
+    function checkGuess() {
+        if (userGuess === randomNumber) {
+            alert("Congratulations! You guessed the correct number.");
+        } else {
+            alert("Sorry, try again!");
+        }
+    }
+
+</script>
+
+<div>
+    <span>The random number is: <strong>{randomNumber}</strong></span>
+</div>
+
+<form onsubmit={checkGuess}>
+    <input type="number" bind:value={userGuess} min="0" max="100">
+    <button type="submit">Submit Guess</button>
+    
+</form>
+<button onclick={resetGame}>Reset</button>
