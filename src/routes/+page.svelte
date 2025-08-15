0cd23627ca70fa4430bb104c0986077d41b817b0
Mathis <mathisjung02@gmail.com>
2025-08-16 00:07:53 +0200

    better feeback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 
     let userGuess: string | null = $state(null);
     let mainInput: HTMLInputElement | null = $state(null);
+    let submitTime: number | null = $state(null); // reactive value of "when" the user press enter
 
     let serverResponse: number | string | null = $state(null);
     let win = $derived(serverResponse === Ordering.EQUAL);
@@ -22,20 +23,21 @@
 
     async function submit(e: SubmitEvent) {
         e.preventDefault();
-        if (!userGuess) return; // add feeback
+        submitTime = Date.now();
+        if (!userGuess) return; // TODO: add feeback
         try {
             serverResponse = await userGuessCommand({
                 targetId: data.randomId,
                 userInput: userGuess,
             });
         } catch (e) {
+            serverResponse = "whoopsie 500"
             console.log(e);
         }
     }
 
     $effect(() => {
-        if (serverResponse) {}; // empty block to trigger effect
-        if (win) {
+        if (submitTime && win) {
             untrack(() => {
                 userGuess = null;
             });
@@ -43,25 +45,43 @@
         } else {
             mainInput?.select();
         }
-    })
+    });
 </script>
 
+{#key submitTime}
+    <div class="z-30 uniqPulse fixed w-full h-full border border-white/20 rounded-2xl pointer-events-none">
+    </div>
+{/key}
+
 <div
-    class=" transition-colors duration-100 relative w-screen h-screen flex flex-col items-center justify-center bg-gray-900
+    class="overflow-hidden transition-colors duration-100 relative w-screen h-screen flex flex-col items-center justify-center
     text-white
-    {win ? 'bg-green-800' : ''}"
+    {win ? 'bg-green-800' : 'bg-gray-900'}"
 >
     <span
-        class="absolute z-0 text-[20rem] -rotate-12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        class="absolute z-0 text-[20rem] -rotate-12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+            {win ? "text-white" : 'text-red-500'}
+        "
     >
         {serverResponseFormatted}
     </span>
     <div
-        class="z-10 w-60 h-80 grid grid-rows-[1fr_4rem] items-center
-            bg-green-900 rounded-2xl shadow overflow-hidden
-            {win ? 'bg-transparent text-green-800' : ''}
+        class="relative z-10 w-60 h-80 grid grid-rows-[1fr_4rem] items-center
+            rounded-2xl shadow-xl overflow-hidden
+            {win ? 'not-focus-within:bg-transparent not-focus-within:text-green-800 backdrop-blur-xs' : ''}
+            {submitTime && !win ? 'focus-within:bg-transparent bg-gray-700 backdrop-blur-md' : ''}
+            transition-all duration-100
+            bg-green-900
         "
     >
+        <!-- {#key userGuess}
+            <div class=" uniqPulse opacity-0 absolute bottom-0 right-0 w-full border-b border-white/40">
+            </div>
+        {/key} -->
+        {#key submitTime}
+            <div class=" uniqPulse opacity-0 absolute bottom-0 right-0 w-full border-b border-white">
+            </div>
+        {/key}
         <div
             class="row-span-1 w-full flex justify-center items-center overflow-hidden
                 {win ? '' : ''}
@@ -93,3 +113,16 @@
         </form>
     </div>
 </div>
+
+<style>
+    @keyframes fadeOut {
+        0% { opacity: 80%; }
+        10% { opacity: 100%;}
+        100% { opacity: 0% }
+    }
+
+    .uniqPulse {
+        opacity: 0%;
+        animation: .3s fadeOut ease-out;
+    }
+</style>
